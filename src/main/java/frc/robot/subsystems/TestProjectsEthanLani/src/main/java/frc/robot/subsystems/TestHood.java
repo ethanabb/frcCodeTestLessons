f1d73ceca53a534bfac68d2525a44b0e3ae2a6ad
ethanabb <ethanabb4@gmail.com>
2025-04-08 12:12:45 -1000

    different ways to run a motor

--- src/main/java/frc/robot/subsystems/TestProjectsEthanLani/src/main/java/frc/robot/subsystems/TestHood.java
+++ src/main/java/frc/robot/subsystems/TestProjectsEthanLani/src/main/java/frc/robot/subsystems/TestHood.java
@@ -1,5 +1,41 @@
 package frc.robot.subsystems;
 
-public class TestHood {
-    // public TalonFX hoodMotor =  
-}
+import java.security.PublicKey;
+
+import org.ejml.dense.row.CommonOps_MT_CDRM;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.DeferredCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
+
+public class TestHood extends SubsystemBase{
+    public TalonFX leadMotor = new TalonFX(0);
+
+
+    public Command setSpeed(){
+        return new InstantCommand(
+          () -> {
+            leadMotor.set(0.50);
+          });
+    }
+
+    public Command stopTheStupidMotor(){
+        return new InstantCommand(
+            () -> {
+                leadMotor.set(0);
+            });
+    }
+
+
+
+    }
+    
+
+
+
