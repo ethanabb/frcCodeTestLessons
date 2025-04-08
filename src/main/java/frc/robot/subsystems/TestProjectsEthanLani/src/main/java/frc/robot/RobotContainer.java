7288c7ead9329c90ca0c9cddb0858945c5a0d3a4
ethanabb <ethanabb4@gmail.com>
2025-04-08 12:13:27 -1000

    dxgcfghjk

--- src/main/java/frc/robot/subsystems/TestProjectsEthanLani/src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/subsystems/TestProjectsEthanLani/src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,7 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
+// import frc.robot.commands.Autos;
 import frc.robot.subsystems.TestHood;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
